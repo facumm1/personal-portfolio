10920203d2854e3cdebe0461592d2074821afbf5
Facundo <facundomamani120@gmail.com>
2025-02-07 13:51:22 -0300

    added fade effect in titles

--- src/components/Titles.tsx
+++ src/components/Titles.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {ReactTyped} from 'react-typed';
 
 import {useTheme} from '@emotion/react';
-import {Box, Typography, useMediaQuery} from '@mui/material';
+import {Box, Fade, Typography, useMediaQuery} from '@mui/material';
 
 import {LaptopDevice, MobileDevice} from './Devices';
 import {Header} from './Header';
@@ -17,49 +17,51 @@ export const Titles: React.FC<{handleMenu: (state: boolean) => void}> = ({
   const isDesktop = useMediaQuery(theme.breakpoints.up('xl'));
 
   return (
-    <Box
-      sx={{
-        width: {xs: '100%', lg: '50%'},
-        position: isLaptop ? 'fixed' : 'relative',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        left: 0,
-      }}>
-      {isLaptop && (
-        <Box sx={{alignSelf: 'flex-start'}}>
-          <Header handleMenu={handleMenu} />
-        </Box>
-      )}
-
-      <Typography
+    <Fade in={true} timeout={1500}>
+      <Box
         sx={{
-          mt: {xs: 3, lg: 1, xl: 5},
-          color: 'secondary.main',
-          fontFamily: 'typography.fontFamily',
-          fontSize: {xs: '32px', xl: '50px'},
-          textAlign: 'center',
-          fontWeight: '800',
+          width: {xs: '100%', lg: '50%'},
+          position: isLaptop ? 'fixed' : 'relative',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          left: 0,
         }}>
-        Facundo Mamani
-      </Typography>
+        {isLaptop && (
+          <Box sx={{alignSelf: 'flex-start'}}>
+            <Header handleMenu={handleMenu} />
+          </Box>
+        )}
 
-      <ReactTyped
-        style={{
-          color: '#E2E8F0',
-          fontFamily: 'typography.fontFamily',
-          fontSize: isDesktop ? '40px' : '28px',
-          textAlign: 'center',
-          fontWeight: '500',
-        }}
-        loop
-        strings={subtitles}
-        typeSpeed={150}
-      />
+        <Typography
+          sx={{
+            mt: {xs: 3, lg: 1, xl: 5},
+            color: 'secondary.main',
+            fontFamily: 'typography.fontFamily',
+            fontSize: {xs: '32px', xl: '50px'},
+            textAlign: 'center',
+            fontWeight: '800',
+          }}>
+          Facundo Mamani
+        </Typography>
 
-      {/* Device frame */}
-      {isLaptop ? <LaptopDevice /> : <MobileDevice />}
-    </Box>
+        <ReactTyped
+          style={{
+            color: '#E2E8F0',
+            fontFamily: 'typography.fontFamily',
+            fontSize: isDesktop ? '40px' : '28px',
+            textAlign: 'center',
+            fontWeight: '500',
+          }}
+          loop
+          strings={subtitles}
+          typeSpeed={150}
+        />
+
+        {/* Device frame */}
+        {isLaptop ? <LaptopDevice /> : <MobileDevice />}
+      </Box>
+    </Fade>
   );
 };
