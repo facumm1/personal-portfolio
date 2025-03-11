724e4f3d8f908e806b9c4623645b5695b123a1b6
Facundo <facundomamani120@gmail.com>
2025-03-11 00:08:14 -0300

    changed cv

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,6 +1,6 @@
 export const personalData = {
   resumeLink:
-    'https://drive.google.com/file/d/1GMgcCle2Vhl-a6Rd6HN9CMF9S6GyPh0t/view?usp=sharing',
+    'https://drive.google.com/file/d/1LwDdHh688YNt5Wc-aHH0bABMuJqDKY5b/view?usp=sharing',
   email: 'facundomamani120@gmail.com',
   github: 'https://github.com/facumm1',
   linkedin: 'https://www.linkedin.com/in/facundo-mamani31',
